c09e72e9e032dcaeb2a15585891df4097e96a7cc
dongja94 <dongja94@gmail.com>
2016-04-20 14:09:48 +0900

    Initial commit

---
+++ app/src/main/java/com/begentgroup/samplerecyclerview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.begentgroup.samplerecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    MyAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView)findViewById(R.id.list);
+        mAdapter = new MyAdapter();
+        recyclerView.setAdapter(mAdapter);
+
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        initData();
+    }
+
+    private void initData() {
+        for (int i = 0; i < 40 ; i++) {
+            mAdapter.add("title : " + i);
+        }
+    }
+}
---
+++ app/src/main/java/com/begentgroup/samplerecyclerview/MyViewHolder.java
@@ -0,0 +1,20 @@
+package com.begentgroup.samplerecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by dongja94 on 2016-04-20.
+ */
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    TextView titleView;
+    public MyViewHolder(View itemView) {
+        super(itemView);
+        titleView = (TextView)itemView.findViewById(R.id.text_title);
+    }
+
+    public void setTitle(String title) {
+        titleView.setText(title);
+    }
+}
